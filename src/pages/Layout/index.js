679a30cd7e471d2a225d8930d94d51efb64d71f1
JagerChoi <17322905680@qq.com>
2026-01-09 10:17:46 +0800

    Add Layout styling and update package.json

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -3,24 +3,63 @@ import {Button} from 'antd-mobile'
 import {useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import {getBillList} from '@/store/modules/billstore'
+import {TabBar} from "antd-mobile"
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
+import './index.scss'
+import {useNavigate} from 'react-router-dom'
+
+
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
 
 const Layout =()=>{
     const dispatch = useDispatch()
     useEffect(()=>{
         dispatch(getBillList())
     },[dispatch])
+
+    //切换菜单跳转路由
+    const navigate=useNavigate()
+const switchRoute=(path)=>{
+    console.log(path)
+    navigate(path)
+}
+
     return (
     
-    <div>
-        <Outlet/>
-        我是layout
-        {/*测试全局生效测试 */}
-        <Button color="primary">测试全局</Button>
-        <div className="purple">
-            {/*测试局部 */}
-            <Button color="primary">测试局部</Button>
+    <div className="layout">
+        <div className="container">
+            <Outlet/>
         </div>
+        <div className="footer">
+            <TabBar onChange={switchRoute}>
+            {tabs.map(item =>(
+                <TabBar.Item key={item.key} icon ={item.icon} titile={item.title}/>
+            ))}
+            
+
+            </TabBar>
         </div>
+    </div>
 )}
 
 export default Layout
